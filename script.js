8b7ef7c7cc1e4715ef00e6b922d79991ad7e2797
NatiOesquer <94532066+NatiOesquer@users.noreply.github.com>
2024-03-15 00:18:29 -0300

    modifico todo.. no me gusta la anterior version (#4)

--- script.js
+++ script.js
@@ -1,101 +1,168 @@
-let botonEncriptar = document.querySelector(".btn-encriptar");
-let botonDesencriptar = document.querySelector(".btn-desencriptar");
-let munieco = document.querySelector(".contenedor-munieco");
-let contenedor = document.querySelector(".contenedor-parrafo");
-let resultado = document.querySelector(".texto-rtdo");
-
-botonEncriptar.onclick = encriptar;
-botonDesencriptar.onclick = desencriptar;
+let salida= "";
+let cajatex = "";
 
 function encriptar(){
-    ocultarAdelante();
-    let cajatexto = recuperarTexto()
-    resultado.textContent = encriptarTexto(cajatexto);
-}
+    salida = "";
+    cajatex = document.querySelector('#cajatexto').value;
+    let texto = cajatex.length;
 
-function desencriptar(){
-    ocultarAdelante();
-    var cajatexto = recuperarTexto()
-    resultado.textContent = desencriptarTexto(cajatexto);
-}
-
-function recuperarTexto(){
-    var cajatexto = document.querySelector(".cajatexto")
-    return cajatexto.value
-}
-
-function ocultarAdelante(){
-    munieco.classList.add("ocultar");
-    contenedor.classList.add("ocultar");
-}
-
-function encriptarTexto(mensaje){
-    let texto = mensaje;
-    let textoFinal = "";
-
-    for(let i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "ai"
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "enter"
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "imes"
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "ober"
-        }
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "ufat"
-        }
-        else{
-            textoFinal = textoFinal + texto[i]
-        }
-    }
-    return textoFinal;
-
-}
-
-function desencriptarTexto(mensaje){
-    let texto = mensaje;
-    let textoFinal = "";
-
-    for(let i = 0; i < texto.length; i++){
-        if(texto[i] == "a"){
-            textoFinal = textoFinal + "a"
-            i = i+1;
-        }
-        else if(texto[i] == "e"){
-            textoFinal = textoFinal + "e"
-            i = i+4;
-        }
-        else if(texto[i] == "i"){
-            textoFinal = textoFinal + "i"
-            i = i+3;
-        }
-        else if(texto[i] == "o"){
-            textoFinal = textoFinal + "o"
-            i = i+3;
-        }
-        
-        else if(texto[i] == "u"){
-            textoFinal = textoFinal + "u"
-            i = i+3;
-        }
-        else{
-            textoFinal = textoFinal + texto[i];
-        }
-        
+    if  (texto == 0) {
+        swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "DEBES INGRESAR ALGUN TEXTO PARA ENCRIPTAR!"
+        });
+        return
     }
 
-    return textoFinal;
+
+document.getElementById("textosalida").removeAttribute("hidden");
+document.getElementById("copia").removeAttribute("hidden");
+
+for (let i = 0; i < texto; i++) {
+
+    let cambio = "";
+
+    sale=cajatex[i];
+
+    if (sale == "a") {
+        cambio = "ai";
+        salida = salida+cambio;
+    } else if (sale =="e"){
+        cambio ="enter";
+        salida = salida+cambio;
+    } else if (sale =="i"){
+        cambio ="imes";
+        salida = salida+cambio;
+    } else if (sale == "o"){
+        cambio = "ober";
+        salida = salida+cambio;
+    } else if (sale == "u"){
+        cambio = "ufat";
+        salida = salida+cambio;
+    }else {
+        salida = salida+sale;
+    }
+}
+document.getElementById('textosalida').value = salida;
+return;
+}
+
+function desencriptar() {
+    salida = "";
+    cajatex = document.querySelector('#cajatexto').value;
+    let texto = cajatex.length;
+
+    
+    if  (texto == 0) {
+        swal.fire({
+            icon: "warning",
+            title: "Oops...",
+            text: "DEBES PONER UN TEXTO PARA SER DESENCRIPTADO!"
+        });
+        return
+    }
+
+    document.getElementById("textosalida").removeAttribute("hidden");
+    document.getElementById("copia").removeAttribute("hidden");
+
+    for (let i = 0; i < texto; i++) {
+
+        let cambio = "";
+
+        sale=cajatex[i];
+
+        if (sale == "a") {
+
+            i++;
+            sale=sale + cajatex[i];
+
+            if (sale=="ai"){
+                sale="a";
+                salida=salida+sale;
+            }
+            salida=salida+cambio;
+
+        }else if (sale == "e"){
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+
+            if (sale=="enter"){
+                sale="e";
+                salida=salida+sale;
+            }
+            salida=salida+cambio;
+
+        }else if (sale == "i"){
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+
+            if (sale=="imes"){
+                sale="i";
+                salida=salida+sale;
+            }
+            salida=salida+cambio;
+
+        }else if (sale == "o"){
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+
+            if (sale=="ober"){
+                sale="o";
+
+                salida=salida+sale;
+            }
+            salida=salida+cambio;
+
+        }else if (sale == "u"){
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+            i++;
+            sale=sale + cajatex[i];
+
+            if (sale=="ufat"){
+                sale="u";
+                salida=salida+sale;
+            }
+            salida=salida+cambio;
+
+        }else{
+            salida=salida+sale;
+        }
+    }
+    document.getElementById('textosalida').value = salida;
+    return;
 
 }
 
-const btnCopiar = document.querySelector(".btn-copiar"); 
-    btnCopiar.addEventListener("click", copiar = () => {
-    let contenido = document.querySelector(".texto-rtdo").textContent;
-    navigator.clipboard.writeText(contenido);
-    //console.log("hola"); 
-});
+function copiartexto(){
+    let content = document.getElementById('textosalida');
+    content.select();
+    document.execCommand('copy');
+    swal.fire({
+        title: "Bien hecho!",
+        text: "Texto Copiado!",
+        icon: "success"
+    });
+    document.getElementById("textosalida").value ="";
+    document.getElementById("cajatexto").value="";
+    textosalida.setAttribute("hidden", "");
+    copia.setAttribute("hidden", "");
+}
+
